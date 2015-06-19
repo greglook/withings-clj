2207eac2f4e3487b5400becff3652a7f248e5b88
Greg Look <greg@greg-look.net>
2015-06-18 22:35:17 -0700

    Remove some unused dependencies.

--- project.clj
+++ project.clj
@@ -9,7 +9,5 @@
   :dependencies
   [[clj-http "1.1.2"]
    [clj-oauth "1.5.2"]
-   [clj-time "0.9.0"]
-   [com.stuartsierra/component "0.2.2"]
    [org.clojure/clojure "1.7.0-RC1"]
    [org.clojure/tools.logging "0.3.1"]])
