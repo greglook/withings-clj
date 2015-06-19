c253b4c522673e4d819a671a7b15407bfba136c5
Greg Look <greg@greg-look.net>
2015-06-19 09:21:29 -0700

    Clean up withings oauth a bit.

--- src/mvxcvi/withings.clj
+++ src/mvxcvi/withings.clj
@@ -8,31 +8,50 @@
     [oauth.client :as oauth]))
 
 
+;; ## OAuth Credentials
+
 (def default-oauth-url
   "https://oauth.withings.com/account")
 
 
-(def default-api-url
-  "https://wbsapi.withings.net/v2")
+(defn oauth-consumer
+  "Constructs a new OAuth 1.0 consumer using the given access and secret key."
+  ([consumer-key consumer-secret]
+   (oauth-consumer consumer-key consumer-secret default-oauth-url))
+  ([consumer-key consumer-secret oauth-url]
+   (oauth/make-consumer
+     consumer-key
+     consumer-secret
+     (str oauth-url "/request_token")
+     (str oauth-url "/access_token")
+     (str oauth-url "/authorize")
+     :hmac-sha1)))
 
 
-(defn init-oauth
-  "Initializes an OAuth 1.0 token for approval by the user. This can then be
-  used to perform API requests."
-  [consumer-access-key consumer-secret-key]
-  (let [consumer (oauth/make-consumer
-                   consumer-access-key
-                   consumer-secret-key
-                   (str default-oauth-url "/request_token")
-                   (str default-oauth-url "/access_token")
-                   (str default-oauth-url "/authorize")
-                   :hmac-sha1)
-        request-token (oauth/request-token consumer nil)] ; no callback URI
-    {:consumer consumer
-     :authz-url (oauth/user-approval-uri consumer (:oauth_token request-token))
-     :access-token (oauth/access-token consumer request-token)}))
+(defn request-access!
+  "POSTs to the request-token endpoint to retrieve a temporary token credential
+  for the end-user to authorize. Returns a map containing the temporary token
+  and a URL for the user to visit to authorize the consumer's access."
+  ([consumer]
+   (request-access! consumer nil))
+  ([consumer callback-url]
+   (let [temp-token (oauth/request-token consumer callback-url)
+         authz-url (oauth/user-approval-uri consumer (:oauth_token temp-token))]
+     {:temp-token temp-token
+      :authz-url authz-url})))
 
 
+(defn authorize-credentials!
+  "POSTs to the access-token endpoint to trade the authorized temporary token
+  credential for a long-term access token credential. This should be called
+  after `request-access!` and the user authorization. Returns a map of
+  credentials that can be serialized and used to authenticate API calls."
+  [consumer temp-token]
+  {:consumer consumer
+   :access-token (oauth/access-token consumer temp-token)})
+
+
+#_
 (defn oauth-credentials
   [consumer access-token method url params]
   (oauth/credentials
@@ -42,3 +61,12 @@
     method ; e.g. :POST
     url
     params))
+
+
+
+;; ## API Access
+
+(def default-api-url
+  "https://wbsapi.withings.net/v2")
+
+
